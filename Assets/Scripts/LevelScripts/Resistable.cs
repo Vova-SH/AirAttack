65943fbada81d99a88bad33e48280b2834f71be7
Vova_SH <vladimir-shperling@yandex.ru>
2018-03-18 01:39:08 +0700

    add units and tower

---
+++ Assets/Scripts/LevelScripts/Resistable.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Resistable {
+	public string tagObject;
+	[Range (0,2)]
+	public float resist = 1f;
+}
---
+++ Assets/Scripts/LevelScripts/WayPoints.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class WayPoints {
+	public GameObject gameObject;
+	public int waitTime = 0;
+}
