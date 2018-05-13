c46e4b3d066fec00ba8c067d3e697602c8c4a616
Vova_SH <vladimir-shperling@yandex.ru>
2018-05-13 22:09:21 +0700

    fix change level in menu

---
+++ Assets/Scripts/CursorScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorScript : MonoBehaviour {
+
+	void Start () {
+		
+	}
+
+	void Update () {
+		Debug.DrawRay (transform.position, transform.forward * 100);
+	}
+}
--- Assets/Scripts/MenuLevelItem.cs
+++ Assets/Scripts/MenuLevelItem.cs
@@ -4,27 +4,47 @@ using UnityEngine.EventSystems;
 
 public class MenuLevelItem : MonoBehaviour
 {
+	public int levelNum;
 	public Text textView;
-
+	public Image imageView;
+	public Color disableColor;
+	public Sprite selectedSprite;
 	public string onSelectText;
+	public TransitionManagerMenu transition;
+
+	private Sprite oftenSprite;
 
 	void Start ()
 	{
-		EventTrigger eventTrigger = GetComponent<EventTrigger> ();
-		if (eventTrigger == null)
-			eventTrigger = gameObject.AddComponent<EventTrigger> ();
-		EventTrigger.Entry pointerEnter = new EventTrigger.Entry ();
-		pointerEnter.eventID = EventTriggerType.PointerEnter;
-		pointerEnter.callback.AddListener ((eventData) => {
-			textView.text = onSelectText;
-		});
+		oftenSprite = imageView.sprite;
+		if (PrefsManager.CompleteLevel < levelNum) {
+			imageView.sprite = selectedSprite;
+			imageView.color = disableColor;
+		} else {
+			EventTrigger eventTrigger = GetComponent<EventTrigger> ();
+			if (eventTrigger == null)
+				eventTrigger = gameObject.AddComponent<EventTrigger> ();
+			EventTrigger.Entry pointerEnter = new EventTrigger.Entry ();
+			pointerEnter.eventID = EventTriggerType.PointerEnter;
+			pointerEnter.callback.AddListener ((eventData) => {
+				textView.text = onSelectText;
+				imageView.sprite = selectedSprite;
+			});
 
-		EventTrigger.Entry pointerExit = new EventTrigger.Entry ();
-		pointerExit.eventID = EventTriggerType.PointerExit;
-		pointerExit.callback.AddListener ((eventData) => {
-			textView.text = null;
-		});
-		eventTrigger.triggers.Add (pointerEnter);
-		eventTrigger.triggers.Add (pointerExit);
+			EventTrigger.Entry pointerExit = new EventTrigger.Entry ();
+			pointerExit.eventID = EventTriggerType.PointerExit;
+			pointerExit.callback.AddListener ((eventData) => {
+				imageView.sprite = oftenSprite;
+			});
+
+			EventTrigger.Entry pointerClick = new EventTrigger.Entry ();
+			pointerExit.eventID = EventTriggerType.PointerClick;
+			pointerExit.callback.AddListener ((eventData) => {
+				transition.LoadScenes(levelNum);
+			});
+			eventTrigger.triggers.Add (pointerEnter);
+			eventTrigger.triggers.Add (pointerExit);
+			eventTrigger.triggers.Add (pointerClick);
+		}
 	}
 }
