9b1024b33c936c19366aefe647ba4e59fa238ef1
Vova_SH <vladimir-shperling@yandex.ru>
2018-05-16 11:10:27 +0700

    add particle player, add cruve ui. add final level menu, fix tower and other minor fix

---
+++ Assets/Scripts/LevelScripts/MenuPosition.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuPosition : MonoBehaviour {
+	public void SetPositionY(Transform anchor)
+    {
+            Vector3 target = anchor.position - transform.position;
+			target.y = transform.forward.y;
+			var angle = Vector3.SignedAngle (target, transform.forward, Vector3.up);
+			transform.Rotate (-Vector3.up, angle, Space.World);
+    }
+}
