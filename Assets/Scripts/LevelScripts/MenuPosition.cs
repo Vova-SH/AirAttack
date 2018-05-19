229f78a418367455ad650cfbcb47a44be2e81bf6
Vova-SH <60854798+Vova-SH@users.noreply.github.com>
2018-05-20 02:07:43 +0700

    replace final menu in level1 and fix menu

--- Assets/Scripts/LevelScripts/MenuPosition.cs
+++ Assets/Scripts/LevelScripts/MenuPosition.cs
@@ -5,9 +5,7 @@ using UnityEngine;
 public class MenuPosition : MonoBehaviour {
 	public void SetPositionY(Transform anchor)
     {
-            Vector3 target = anchor.position - transform.position;
-			target.y = transform.forward.y;
-			var angle = Vector3.SignedAngle (target, transform.forward, Vector3.up);
-			transform.Rotate (-Vector3.up, angle, Space.World);
+            var rot = anchor.rotation.eulerAngles;
+			transform.rotation = Quaternion.Euler(0, rot.y, 0);
     }
 }
